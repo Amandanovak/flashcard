8e49ca2532a6b2892d64c90cf7f6ec4a1ee37aab
Amandanovak <amanda.nascimento.novak@escola.pr.gov.br>
2025-06-17 23:09:18 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Ano de fundação?',
+    '1910'
+)
+
+criaCartao(
+    'Corinthians',
+    'Estádio atual?',
+    'Neo Química Arena'
+)
+
+criaCartao(
+    'Corinthians',
+    'Mascote do time?',
+    'Mosqueteiro'
+)
+
+criaCartao(
+    'Corinthians',
+    'Cores do time?',
+    'Preto e branco'
+ )
